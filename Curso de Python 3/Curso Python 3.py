3d544015113f048b075ee30317a18898bd2e6b58
andreslmd <andreslmd@me.com>
2022-07-14 19:54:43 -0300

    Windows

--- Curso de Python 3/Curso Python 3.py
+++ Curso de Python 3/Curso Python 3.py
@@ -1,7 +1,7 @@
 #CURSO PYTHON 3
 
 #Hola a todos comenzare con este pequeño curso de Python es la forma donde estare enseñando lo poco que ire aprendiendo.
-
 #. 
 
+#NO PUEDE DEJAR DE FALTA FAMOSO:
 print('Hello, World!')
