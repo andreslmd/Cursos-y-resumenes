6ef0205af5d605ffaff500963bd88e2c21cd4911
andreslmd <andreslmd@me.com>
2022-07-15 01:00:28 -0300

    Parte 1, 2,3 y4

--- Curso de Python 3/Curso Python 3.py
+++ Curso de Python 3/Curso Python 3.py
@@ -8,3 +8,48 @@
 # NO PUEDE FALTAR EL FAMOSO:
 print('Hello, World!')
 # Para comenzar esta aventura.
+
+
+#-------------------- Parte 1 
+
+# Que es Python?
+# Python es un lenguaje de programación interpretado cuya filosofía hace hincapié en la legibilidad de su código.
+# Es un leguaje multiparadigma por que pose:
+# 1. Programación Orientado a Objetos, 2. Programación Interactiva y 3. Programación Funcional
+# Python posee las siguientes 4 cualidades: 1. Interpretado, 2. Dinámico, 3. multiplataforma y 4. multiparadigma.
+
+# Python como un lenguaje interpretado: Un lenguaje es interpretado cuando el intérprete del lenguaje (es decir, el programa que lo implementa) ejecuta las instrucciones una a una, sin hacer compilación previa.
+# Esto se diferencia de los lenguajes compilados para los cuales un compilador primero lee el programa completo y luego lo traduce a otra representación, que está pensada para ser ejecutada.
+# Pero existe una definición más amplia de “interpretado” la cual un lenguaje es interpretado si no lo ejecuta directamente el hardware de la máquina, sino los que son ejecutados por una “máquina virtual” el cual es un programa que simula un CPU.
+
+# Python como un lenguaje Dinámico: Python posee un tipado dinámico, en lugar de estático. En los lenguajes estáticos, cuando declaramos una variable es necesario especificar el tipo que tiene y dicho tipo de dato es inalterable.
+# En el lenguaje Python no es necesario establecer un Tipo y, además, puede cambiar en cualquier momento, ya que viene determinado en tiempo de ejecución por el valor de la variable.
+# Ejemplo
+arboles = 'Roble'
+
+# Python como un lenguaje multiplataforma: ya que puede ejecutarse su código fuente entre varios sistemas operativos (macOS, Windows, Linux). 
+# Existen varias implementaciones alternativas que están disponibles: IronPython (.NET)  Jython. Ejecutado en el Java Virtual Machine.  PyPy. Implementación de Python en compilador JIT.  Stackless Python.  MicroPython. Se ejecuta Python en microcontroladores.
+
+# Los principios de Python: Existen 20 principios de Software que influyen en Python y 19 de estos fueron escritos por Tim Peters.
+# Los principios son: Explícito es mejor que implícito.  Simple es mejor que complejo.  Bello es mejor que feo.  Complejo es mejor que complicado.  
+# Plano es mejor que anidado.  Disperso es mejor que denso.  Legibilidad cuenta.  Práctico es mejor que puro.  
+# Casos especiales no son tan especiales como para quebrantar la regla.  Los errores no deben dejarse pasar silenciosamente.  
+# A menos que hayan sido silenciados explícitamente.  Frente a la ambigüedad, rechaza la tentación de adivinar.  Debería haber una, y solo una, manera obvia de hacerlo.  Ahora es mejor que nunca.  
+# Aunque nunca es a veces mejor que ya mismo.  Si la implementación es difícil de explicar, es una mala idea.  Si la implementación es fácil de explicar, quizás sea una buena idea.  
+# Los namespaces son una gran idea.  Plano es mejor que anidado.
+
+# Se debe elegir Python por su sencilles(creado para principiante, sintaxys fácil de comprender), codigo abierto(gratuito y código abierto desde hace 20 años),
+# Popular(Es popular y se crean miles de librerías, módulos, códigos y programas), multidisciplinario(Varios usos entre Dllo Web, big data, IA, videojuegos, finanzas, etc. Esto te permite
+# elegir el sector que más te guste y la mejor oferta laboral).
+
+# Ventajas de Python: Facilidad de uso.  Legibilidad de código.  Facilidad de escritura de código asíncrono.  Muchas bibliotecas disponibles.
+# Facilidad de uso.  Legibilidad de código.  Facilidad de escritura de código asíncrono.  Muchas bibliotecas disponibles.
+
+# Desventajas de Python: Tiene mas ventajas que desventajas pero estas son: Curva de aprendizaje: la curva de aprendizaje en la parte web no es fácil.  Hosting: la mayoría de los servidores no tienen soporte a Python, pero si lo soportan, la configuración tampoco es sencilla.  
+# Librerías incluidas: algunas librerías incluidas no son del gusto de la comunidad y prefieren librerías de terceros.
+
+
+#-------------------- Parte 2 
+
+
+
