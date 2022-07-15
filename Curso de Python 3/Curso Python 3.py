55d63971e8dcc60631989fd4aebb48cf76157543
andreslmd <andreslmd@me.com>
2022-07-15 01:03:44 -0300

    Part 1,2,3 y 4

--- Curso de Python 3/Curso Python 3.py
+++ Curso de Python 3/Curso Python 3.py
@@ -51,5 +51,124 @@ arboles = 'Roble'
 
 #-------------------- Parte 2 
 
+#Breve historia de Python:
+# Python fue creado hacia finales de los años 80s y su implementación comenzó en diciembre de 1989 cuando Guido Van Rossum que 
+# trabajaba en CWI, centro de investigación holandés de carácter oficial que, entre otras cosas, actualmente alberga la oficina 
+# central de W3C, decidió empezar el proyecto dándole continuidad al lenguaje de programación ABC del que había formado parte 
+# del equipo de desarrollo de CWI. 
+
+#El nombre Python viene dado por la afición de Van Rossum al grupo Monty Python y fue originalmente desarrollado para 
+# el sistema operativo Amoeba.
+
+#Versiones:
+# Hay 3 versiones, la mas reciente la versión 3. 
+# La transición de Python 2 a Python 3 ha resultado mucho más costosa de lo esperado, debido a que Python 3 introdujo muchos cambios 
+# en el lenguaje y obligaba a reescribir prácticamente todos los programas (aunque se han creado herramientas para ayudar en ese proceso).
+
+# Python cuenta con un gran número de bibliotecas, cuyo repositorio oficial es PyPI (Python Package Index).
+
+# En julio de 2018, Guido van Rossum anunció su retirada como BDOF.
 
 
+#-------------------- Parte 3 
+
+# Jupyter Notebook y Spyder:
+# Jupyter Notebook se utiliza en el análisis de datos y es una herramienta muy conocida y utilizada en el mundo. 
+# El problema es que Anaconda utiliza mucho espacio.
+# Se descarga desde anaconda.com. Anaconda utiliza mucho espacio por ese motivo puedes usar miniconda(version reducida).
+# posterior a esto se instala Jupyterlab que es la suite que contiene Jupyter Notebook. 
+# Después de instalar Jupyter, se pasa a instalar Spyder será nuestro entorno de desarrollo en Python.
+
+
+#-------------------- Parte 4
+# Nomenclatura de Python: 
+#Parte 4 
+# Nomenclatura de Python.
+# En Python tenemos las PEP’s o Python Enhancement Proposals, es decir, documentos que proveen información a la comunidad 
+# y describen nuevas características, su proceso de creación o su entorno. Entre estas PEP’s tenemos la PEP 8, dedicada 
+# a la recopilación de los estándares seguidos por los desarrolladores de Python a la hora de escribir código para la librería estándar.
+
+#Indetación:
+# Se recomienda usar siempre 4 espacios y nunca mezclar tabuladores o espacios. Si se continúa una línea hay 2 opciones: Por ejemplo:
+# Opción 1:
+Si: # opcion 1      
+    foo = funcion_que_crea_bar(variable_1, variable2 
+             variable_3)      
+# opcion 2      
+    foo = funcion_que_crea_bar(
+                        variable_1, variable2
+                        variable_3)
+
+#Tamaño de la línea:
+# Las líneas deben limitarse a un máximo de 79 caracteres.
+
+#Líneas en blanco:
+#Separar las definiciones de las clases y funciones con dos líneas en blanco. Los métodos dentro de clases se separan con una 
+# línea en blanco. Se recomienda utilizar líneas en blanco para separar partes del código, por ejemplo, dentro de una función, 
+# que realizan tareas diferenciadas.
+
+#Imports:
+# Los imports de distintos módulos deben estar en líneas diferentes:  
+#Si: 
+import os  
+    import sys  
+#No: 
+import os, sys  
+
+#Sí se pueden poner en una línea los elementos que se importan de un mismo módulo:  
+from subprocess import Popen, PIPE  
+
+#Los imports deben ponerse siempre al principio del fichero, justo después de los comentarios y de la documentación y 
+# antes de la definición de las variables globales y las constantes.  
+# Los imports deben agruparse en el siguiente orden:  1.Librerías estándar, 2. Librerías de terceros 3. Librerías locales. 
+# Cada grupo de imports debe estar separado por una línea en blanco.
+
+#Espacios en blanco en expresiones:
+#Evitar espacios en blanco extra en:  
+
+# Dentro de paréntesis.  
+# Si:  
+spam(ham[1], {eggs: 2})  
+# No:  
+spam( ham[ 1 ], { eggs: 2 })  
+
+#Después de una coma.  
+# Si: 
+if x == 4:  
+    print x, y; x, y = y, x  
+#No: 
+if x == 4 :  
+    print x , y ; x , y = y , x
+
+#Antes del paréntesis de una llamada a una función.  
+#Si:  
+spam(1)  
+#No: 
+spam (1)  
+
+#Antes del paréntesis de un índice.  
+# Si:  
+dict['key'] = list[index]  
+# No:  
+dict ['key'] = list [index]
+
+#Siempre envolver estos operadores binarios con un espacio a cada lado: asignación (=), incremento en asignación (+=, -= etc.), 
+# comparaciones (==, <, >, !=, <>, <=, >=, in, not in, is, is not), Booleanos (and, or, not).  
+# Usar espacios alrededor de los operadores aritméticos:  
+# Si:  
+i = i + 1  
+    submitted += 1  
+    x = x * 2 - 1  
+    hypot2 = x * x + y * y  
+    c = (a + b) * (a - b)  
+# No:  
+i=i+1  
+    submitted +=1  
+    x = x*2 - 1  
+    hypot2 = x*x + y*y  
+    c = (a+b) * (a-b)  
+
+# Si quieres llegar a ser gran programador de Python no olvidar de estas sencillas normas. Mejorarán la legibilidad y calidad de tu código.
+
+
+#-------------------- Parte 5
