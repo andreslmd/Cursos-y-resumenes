94f25cdd2dead07234de804048ea6735d7ea34b8
andreslmd <andreslmd@me.com>
2022-07-25 16:51:36 -0300

    Curso nuevo

---
+++ Curso de Python 3/Parte 1 - Conceptos iniciales.py
@@ -0,0 +1,11 @@
+# Que es programa?
+# Un programa es un conjunto de instrucciones o pasos a seguir que se le dan a un ordenador de forma secuencial para que realice una tarea específica.
+# El flujo normal de un programa es el siguiente: 
+#   1. El programa recibe datos de entrada, normalmente introducidos por los usuarios de éste.  
+#   2. Ejecuta las instrucciones especificadas por el programador.  
+#   3. El programa obtiene como resultado un conjunto de datos de salida. 
+        #Datos de entrada ----> programa ------> Datos de salida.
+
+# Que es programar?
+# La acción de decirle a un ordenador exactamente lo que tiene que hacer y cómo lo tiene que hacer utilizando un lenguaje de programación específico.
+# Los lenguajes de programación permiten a los programadores transformar la idea que tienen del programa en un conjunto de instrucciones que el ordenador es capaz de ejecutar.
