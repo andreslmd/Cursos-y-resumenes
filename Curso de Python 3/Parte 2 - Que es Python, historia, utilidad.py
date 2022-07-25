3b6ea1930fd2e9311788fad85a50471f3473ecf9
andreslmd <andreslmd@me.com>
2022-07-25 17:50:20 -0300

    Parte 2

---
+++ Curso de Python 3/Parte 2 - Que es Python, historia, utilidad.py
@@ -0,0 +1,117 @@
+# QUE ES PYTHON:
+# Python es un lenguaje de programación interpretado cuya filosofía hace hincapié en la legibilidad de su código.
+# Es un leguaje multiparadigma por que pose:
+# 1. Programación Orientado a Objetos, 2. Programación Interactiva y 3. Programación Funcional
+
+# Las características del lenguaje son las siguiente:
+#   - Simplicidad: ¡La gran fortaleza de Python!  
+#   - Sintaxis clara: La sintaxis de Python es muy clara, es obligatoria la utilización de la indentación en todo el código que se escribe. Gracias a esta característica todos los programas 
+#   escritos en Python tienen la misma apariencia.  
+#   - Propósito general: Se pueden crear todo tipo de programas, incluyendo páginas web.  
+#   - Lenguaje interpretado: Al ser un lenguaje interpretado no es necesario compilarlo, lo que te ahorrará tiempo a la hora de desarrollar. También implica que su ejecución sea más lenta, 
+#   ya que los programas son ejecutados por el intérprete de Python en vez de ejecutados por la máquina donde lo arrancas.  
+#   - Lenguaje de alto nivel: No es necesario que te preocupes de aspectos de bajo nivel como puede ser el manejo de la memoria del programa.  
+#   - Lenguaje orientado a objetos: Lenguaje construido sobre objetos que incorporan datos y funcionalidades.  
+#   - Open Source: Python ha sido portado a los diferentes sistemas operativos, por lo que puedes usarlo en el que más te guste. Otra característica de ser Open Source es que es un 
+#   lenguaje de programación gratuito.  
+#   - Extensas librerías: Facilitan la programación al incorporar mediante librerías una gran cantidad de funcionalidades.  
+#   - Incrustable: Es posible añadir programas escritos en Python a programas escritos en C y C++.
+
+# ¡Python es un lenguaje de programación muy completo! De todas las características que tiene, la clave de su gran éxito es la primera de ellas, la simplicidad con la que cuenta, que lo hace 
+# perfecto para empezar en el mundo de la programación.
+
+# Se debe elegir Python por su sencilles(creado para principiante, sintaxys fácil de comprender), codigo abierto(gratuito y código abierto desde hace 20 años),
+# Popular(Es popular y se crean miles de librerías, módulos, códigos y programas), multidisciplinario(Varios usos entre Dllo Web, big data, IA, videojuegos, finanzas, etc. Esto te permite
+# elegir el sector que más te guste y la mejor oferta laboral).
+
+# Ventajas de Python: Facilidad de uso.  Legibilidad de código.  Facilidad de escritura de código asíncrono.  Muchas bibliotecas disponibles.
+# Facilidad de uso.  Legibilidad de código.  Facilidad de escritura de código asíncrono.  Muchas bibliotecas disponibles.
+
+# Desventajas de Python: Tiene mas ventajas que desventajas pero estas son: Curva de aprendizaje: la curva de aprendizaje en la parte web no es fácil.  Hosting: la mayoría de 
+# los servidores no tienen soporte a Python, pero si lo soportan, la configuración tampoco es sencilla.  
+# Librerías incluidas: algunas librerías incluidas no son del gusto de la comunidad y prefieren librerías de terceros.
+
+
+#------------------
+#Breve historia de Python:
+# Python fue creado hacia finales de los años 80s y su implementación comenzó en diciembre de 1989 cuando Guido Van Rossum que 
+# trabajaba en CWI, centro de investigación holandés de carácter oficial que, entre otras cosas, actualmente alberga la oficina 
+# central de W3C, decidió empezar el proyecto dándole continuidad al lenguaje de programación ABC del que había formado parte 
+# del equipo de desarrollo de CWI. 
+
+#El nombre Python viene dado por la afición de Van Rossum al grupo Monty Python y fue originalmente desarrollado para 
+# el sistema operativo Amoeba.
+
+#Versiones:
+# Hay 3 versiones, la mas reciente la versión 3. 
+# La transición de Python 2 a Python 3 ha resultado mucho más costosa de lo esperado, debido a que Python 3 introdujo muchos cambios 
+# en el lenguaje y obligaba a reescribir prácticamente todos los programas (aunque se han creado herramientas para ayudar en ese proceso).
+
+# Python cuenta con un gran número de bibliotecas, cuyo repositorio oficial es PyPI (Python Package Index).
+
+# En julio de 2018, Guido van Rossum anunció su retirada como BDOF.
+
+
+#------------------
+# El ZEN de Python:
+# La filosofía del lenguaje Python está plasmada en el documento escrito por Tim Peters que puedes encontrar en https://www.python.org/dev/peps/pep-0020/.  
+# A continuación, encontrarás los mantras de Python traducidos al castellano: 
+#   - Hermoso es mejor que feo.  
+#   - Explícito es mejor que implícito.  
+#   - Simple es mejor que complejo.  
+#   - Complejo es mejor que complicado.  
+#   - Sencillo es mejor que anidado.  
+#   - Escaso es mejor que denso.  
+#   - La legibilidad cuenta.  
+#   - Los casos especiales no son lo suficientemente especiales para romper las reglas.  
+#   - Lo práctico le gana a la pureza.  
+#   - Los errores no deben pasar en silencio.  
+#   - A menos que sean silenciados.  
+#   - Respecto a la ambigüedad, rechazar la tentación de adivinar.  
+#   - Debe haber una – y preferiblemente sólo una – manera obvia de hacerlo.  
+#   - Aunque esa manera puede no ser obvia en un primer momento a menos que seas holandés.  
+#   - Ahora es mejor que nunca.  
+#   - Aunque “nunca” es a menudo mejor que “ahora mismo”.  
+#   - Si la aplicación es difícil de explicar, es una mala idea.
+#   - Si la aplicación es fácil de explicar, puede ser una buena idea.  
+#   - Los espacios de nombres son una gran idea ¡hay que hacer más de eso!
+
+
+#------------------
+# Por qué Python?
+# Existen multitud de razones por las cuales debes de aprender a programar en Python.
+
+# Simplicidad:
+# - La característica principal es Python es que es un lenguaje simple, reduce considerablemente el número de líneas de código en comparación con otros lenguajes y 
+# provee herramientas para realizar operaciones de forma más simple que como se realizan con otros lenguajes. 
+# - Por la simplicidad los errores que un programador pueda cometer cuando realiza los programa se ven reducidos, ya que al tener que escribir menos código fuente 
+# se verá reducida la probabilidad de cometer errores.
+# - Al escribir menos líneas de código el tiempo de desarrollo se ve reducido, y ésto es algo muy importante a tener en cuenta cuando se realizan proyectos de desarrollo de software.
+
+# Resultados rápidos:
+# - Con Python vas a poder ver los resultados de forma inmediata.
+# - Python va a permitirte estar haciendo programas a los pocos días (incluso horas) de haber empezado, observarás que avanzas casi sin esfuerzo a gran velocidad.
+
+# Punto de partida:
+# - Python es un lenguaje muy completo, no pienses que por ser simple es un lenguaje básico. Con Python vas a aprender todos los conceptos existentes en el mundo de la programación.
+
+# Librerías:
+# - Python es un lenguaje poderoso. A medida que te vas familiarizando con el lenguaje y vas aprendiendo y manejando todas las funcionalidades descubres que Python dispone de un 
+# conjunto de librerías y módulos muy extenso que te permiten realizar cualquier tipo de proyecto, con total independencia de su naturaleza.
+
+# Desarrollo web:
+# - Existen multitud de frameworks que utilizan Python para el desarrollo web, entre ellos, destaca Django.
+
+# Raspberry Pi: 
+# Python es el lenguaje principal de programación de Raspberry. 
+
+# Comunidad:
+# - La comunidad que hay detrás de este lenguaje de programación es inmensa, lo que provoca que el lenguaje no quede obsoleto y vaya recibiendo actualizaciones. 
+# Otro punto fuerte de la comunidad que tiene detrás es la creación de frameworks, módulos, extensiones y multitud de herramientas que facilitan el desarrollo con este lenguaje. 
+# Los desarrolladores en Python son los primeros interesados en que haya más gente que programe con Python, ya que, de esta forma, el número de herramientas/frameworks que facilitan 
+# el desarrollo será mayor. Una de las cosas más importantes para alguien que empieza con un lenguaje de programación es la ayuda que ofrece la comunidad que tiene alrededor el 
+# lenguaje de programación. Si te animas a aprender Python verás como podrás encontrar sin dificultad la resolución de tus preguntas/dudas/problemas. 
+# - ¡Programando en Python nunca te vas a sentir sólo!
+
+# Demanda laboral alta 
+# - Python es utilizado por las grandes empresas tecnológicas del mundo… Saber Python implicará tener más posibilidades de encontrar ese trabajo que siempre has querido tener.
