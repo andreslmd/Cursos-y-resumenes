9b36ca5fb22f178644b95248b4ce2d450f400d39
andreslmd <andreslmd@me.com>
2022-07-25 18:36:10 -0300

    Parte 3

--- Curso de Python 3/Parte 1 - Conceptos iniciales.py
+++ Curso de Python 3/Parte 1 - Conceptos iniciales.py
@@ -9,3 +9,16 @@
 # Que es programar?
 # La acción de decirle a un ordenador exactamente lo que tiene que hacer y cómo lo tiene que hacer utilizando un lenguaje de programación específico.
 # Los lenguajes de programación permiten a los programadores transformar la idea que tienen del programa en un conjunto de instrucciones que el ordenador es capaz de ejecutar.
+
+
+#----------------------------
+# IDE y/o EDITORES DE CODIGO:
+# Referirnos a IDE, hablamos de un entorno de desarrollo capaz de proporcionar un amplio abanico de características como codificación, compilación, depuración, ejecución, autocompletar, etc. 
+# Mientras que el editor de código simplemente es una plataforma desde al que poder editar y modificar solamente el código, ayudando al programador a escribir scripts, modificar el texto, etc. 
+# Por su parte con el IDE e posible realizar otras funciones, así como ejecutar el código, controlar la versión, etc
+
+#### NOTA IMPORTANTE 2: Hay muchas Entornos de desarrollos entre los más conocidos no voy a decir los mejores ya que a veces depende de los gustos de las personas estan Visual Studio Code (a mi concepto 
+#   el mejor para empezar), PyCharm (Muy interesante pero no me cautivo), Spyder (muy potente pero mas orientedo al lenguaje científico y para ingenieros), Atom(propuesta de GitHub pero después de la 
+#   compra por parte de Microsoft tiene fecha de vencimiento ya que Visual es la joya de la corona), NeoVim o Vim(primero NeoVim es el futuro, muy interante, es un hoja de papel en blanco que debes 
+#   moldear pero se necesita tiempo, dedicación y si eres un usuario nuevo te va costar algo de sacrificio, lo use pero Visual por su facilidad me cautivo) y por último y muy potente Jupyter Notebook
+#   (viene en el paquete Conda, lo use y su forma me gusto, muy fácil de usar y la otro es su base en block de notas, lo uni en VSC y encontre mi lugar favorito).
---
+++ Curso de Python 3/Parte 3 - Entorno de desarrollo, instalación.py
@@ -0,0 +1,19 @@
+# ENTORNO DE DESARROLLO
+# - Python posee un entorno de desarrollo llamado IDLE (Integrated DeveLopment Environment o Integrated Development and Learning Environment).
+# - El entorno de desarrollo está incluido en Python desde la versión 1.5 y está pensado para ser utilizado como entorno de aprendizaje gracias a su simplicidad.
+
+
+#-----------------
+# Para instalación de Python:
+# 1. Ingresar a https://www.python.org.
+# 2. Navega en la sección Downloads
+# 3. Selecciona tu sistema operativo (SO): Windows, macOS, Linux.
+# 4. Descarga la última versión de Python a la fecha de hoy (Julio/2022) es la 3.10.5
+# NOTA IMPORTANTE: Si instalaste Conda la vesión de Python que trae es la versión 3.9.13 la cual es mejor para usar en Jupyter notebook.
+# 5. Instalación: Es ejecutar el instalador, aceptar los terminos, elegir el usuario administrador y completar el instalador.
+# 6. Vas a ver el IDLE de Python.
+# NOTA IMPORTANTE 2: Hay muchas Entornos de desarrollos entre los más conocidos no voy a decir los mejores ya que a veces depende de los gustos de las personas estan Visual Studio Code (a mi concepto 
+#   el mejor para empezar), PyCharm (Muy interesante pero no me cautivo), Spyder (muy potente pero mas orientedo al lenguaje científico y para ingenieros), Atom(propuesta de GitHub pero después de la 
+#   compra por parte de Microsoft tiene fecha de vencimiento ya que Visual es la joya de la corona), NeoVim o Vim(primero NeoVim es el futuro, muy interante, es un hoja de papel en blanco que debes 
+#   moldear pero se necesita tiempo, dedicación y si eres un usuario nuevo te va costar algo de sacrificio, lo use pero Visual por su facilidad me cautivo) y por último y muy potente Jupyter Notebook
+#   (viene en el paquete Conda, lo use y su forma me gusto, muy fácil de usar y la otro es su base en block de notas, lo uni en VSC y encontre mi lugar favorito).
